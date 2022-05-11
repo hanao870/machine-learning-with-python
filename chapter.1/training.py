79749746e7e0394913afb6027c051415c4806ddf
N.Miki <hanackholehole@gmail.com>
2022-05-11 21:15:19 +0900

    [add] アイリスデータを用いた学習モジュール

---
+++ chapter.1/training.py
@@ -0,0 +1,14 @@
+"""訓練データとテストデータ."""
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris_dataset = load_iris()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset["data"], iris_dataset["target"], random_state=0
+)
+
+print(f"X_train shape: {X_train.shape}")
+print(f"y_train shape: {y_train.shape}")
+print(f"X_test shape: {X_test.shape}")
+print(f"y_test shape: {y_test.shape}")
